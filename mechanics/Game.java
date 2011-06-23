35a97f6c2b2cdff944e1ed041103c5864110bab8
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2011-06-24 01:59:04 +0200

    Packet Struktur

---
+++ mechanics/Game.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mechanics;
+
+/**
+ *
+ * @author Eagle
+ */
+public class Game {
+
+}
