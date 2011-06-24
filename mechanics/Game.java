ebb8924eb0d89ddf7b5e819d9c3ef15a26b3c506
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2011-06-24 02:13:16 +0200

    Das absolute Grundgeruest (weniger geht nicht)
    aber vermutlich praktisch um sich vorstellen zu koennen wie das ganze ablaufen wird
    Game.java in mechanics

--- mechanics/Game.java
+++ mechanics/Game.java
@@ -1,14 +1,40 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * The Main Class to handle playing any concrete level
  */
 
 package mechanics;
 
 /**
  *
- * @author Eagle
+ * @author S.D.Eagle
  */
 public class Game {
+
+    private long oldTime;
+    private long difTime;
+
+    public Game() {
+        // load Stuff
+    }
+
+    /**
+     * Actually running the instance
+     */
+    private void run() {
+        boolean exit = false;
+        while (!exit) {
+            difTime = System.currentTimeMillis() - oldTime;
+            //get input
+            //react on input
+            move(difTime);
+            //draw();
+
+            oldTime += difTime;
+        }
+    }
+
+    private void move(long time) {
+        
+    }
 
 }
--- mosod_bounce/Main.java
+++ mosod_bounce/Main.java
@@ -5,6 +5,8 @@
 
 package mosod_bounce;
 
+import mechanics.Game;
+
 /**
  *
  * @author Eagle
@@ -15,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Game();
     }
 
 }
