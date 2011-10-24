d5b9b62d1f9c3a58cd93da0c7ab8b5350c9a920a
Moritz Baumann <flamingmo@online.de>
2011-10-25 01:34:59 +0200

    Allegro 5 test

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,33 @@
  * Created on 22. Oktober 2011, 19:10
  */
 
-#include <cstdlib>
-#include <allegro.h>
-
-using namespace std;
+#include <cstdio>
+#include <allegro5/allegro.h>
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+	ALLEGRO_DISPLAY *display = NULL;
 
-    allegro_init();
-    allegro_message("Allegro works!");
-    return 0;
-    
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+
+	display = al_create_display(640, 480);
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		return -1;
+	}
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_flip_display();
+
+	al_rest(10.0);
+
+	al_destroy_display(display);
+	return 0;
 }
-END_OF_MAIN();
+//END_OF_MAIN();
