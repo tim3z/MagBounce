9029db09fdff6e33c9d882859019a838499ef249
Tim Zeitz <tim.zeitz@googlemail.com>
2011-10-31 21:58:03 +0100

    now using allegro for timing

--- src/AppStates/Game.cpp
+++ src/AppStates/Game.cpp
@@ -1,7 +1,6 @@
 #include "Game.h"
 #include "../Graphics/Graphics.h"
 #include <stdio.h>
-#include <sys/time.h>
 
 // Constructors/Destructors
 //  
@@ -23,12 +22,12 @@ Game::Game () {}
 
 void Game::execute() {
     int passed;
-    timeval lastTime, currentTime;
-    gettimeofday(&lastTime, 0);
+    double lastTime, currentTime;
+    lastTime = al_get_time();
     
     while (!exit) {
-        gettimeofday(&currentTime, 0);
-        passed = (currentTime.tv_usec - lastTime.tv_usec) / 1000; // miliseconds
+        currentTime = al_get_time();
+        passed = (currentTime - lastTime) * 1000; // miliseconds
         lastTime = currentTime;
         
         currentState->reactOnInput();
