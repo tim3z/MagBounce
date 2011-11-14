8c3fb993bdd27bd3c3ca9977249f8a519190f81e
Moritz Baumann <flamingmo@online.de>
2011-11-14 22:03:41 +0100

    Added length calculation and normalization functions to Vector class and made it cache the calculated length.

--- src/Physics/Vector.cpp
+++ src/Physics/Vector.cpp
@@ -1,5 +1,19 @@
 #include "Vector.h"
+#include <math.h>
 
-template<typename T> inline T Vector<T>::length_squared() const {
-    return *this * *this;
+// TODO: what happens for a Vector<int>?
+template<class T> T Vector<T>::length() const {
+    if (length_dirty) {
+        cached_length = sqrt(*this * *this);
+        length_dirty = false;
+    }
+    return cached_length;
+}
+
+template<class T> Vector<T> Vector<T>::normalized() const {
+    return *this / this->length();
+}
+
+template<class T> void Vector<T>::normalize() {
+    *this /= this->length();
 }
--- src/Physics/Vector.h
+++ src/Physics/Vector.h
@@ -8,30 +8,36 @@
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
 
-template <typename T>
+template <class T>
 struct Vector {
+private:
     T x, y;
+    T cached_length;
+    bool length_dirty;
 
-    Vector() : x(0), y(0) {}
+public:
+    Vector() : x(0), y(0), cached_length(0), length_dirty(false) {}
 
-    Vector(const Vector<T>& v) : x(v.x), y(v.y) {}
+    Vector(const Vector<T>& v) : x(v.x), y(v.y), cached_length(0), length_dirty(true) {}
 
-    Vector(const T x, const T y) : x(x), y(y) {}
+    Vector(const T x, const T y) : x(x), y(y), cached_length(0), length_dirty(true) {}
 
     const Vector<T>& operator=(const Vector<T>& v) {
         x = v.x;
         y = v.y;
+        cached_length = v.cached_length;
+        length_dirty = v.length_dirty;
         return *this;
     }
 
     ~Vector() {}
 
 
-    bool operator==(const Vector<T>& v) {
+    bool operator==(const Vector<T>& v) const {
         return (x == v.x && y == v.y);
     }
 
-    bool operator!=(const Vector<T>& v) {
+    bool operator!=(const Vector<T>& v) const {
         return (x != v.x || y != v.y);
     }
 
@@ -51,12 +57,14 @@ struct Vector {
     const Vector<T>& operator+=(const Vector<T>& v) {
         x += v.x;
         y += v.y;
+        length_dirty = true;
         return *this;
     }
 
     const Vector<T>& operator-=(const Vector<T>& v) {
         x -= v.x;
         y -= v.y;
+        length_dirty = true;
         return *this;
     }
 
@@ -72,12 +80,14 @@ struct Vector {
     const Vector<T>& operator*=(const T scalar) {
         x *= scalar;
         y *= scalar;
+        length_dirty = true;
         return *this;
     }
 
     const Vector<T>& operator/=(const T scalar) {
         x /= scalar;
         y /= scalar;
+        length_dirty = true;
         return *this;
     }
 
@@ -87,7 +97,50 @@ struct Vector {
     }
 
 
-    inline T length_squared() const;
+    T getX() const {
+        return x;
+    }
+
+    T getY() const {
+        return y;
+    }
+
+    void setX(const T newX) {
+        x = newX;
+        length_dirty = true;
+    }
+
+    void setY(const T newY) {
+        y = newY;
+        length_dirty = true;
+    }
+
+    /**
+     * Calculates the length of this vector.
+     *
+     * The behavior of this function on a Vector<int> is currently undefined.
+     *
+     * @return Length of this vector
+     */
+    T length() const;
+    /**
+     * Creates a normalized version of this vector.
+     *
+     * It is not guaranteed that the returned vector's length is exactly 1, due to possible rounding errors.
+     * Be sure to check whether this vector's length is 0 before calling this function.
+     * The behavior of this function on a Vector<int> is currently undefined.
+     *
+     * @return Normalized vector
+     */
+    Vector<T> normalized() const;
+    /**
+     * Normalizes this vector.
+     *
+     * It is not guaranteed that this vector's length is exactly 1 afterwards, due to possible rounding errors.
+     * Be sure to check whether this vector's length is 0 before calling this function.
+     * The behavior of this function on a Vector<int> is currently undefined.
+     */
+    void normalize();
 };
 
 #endif /* _VECTOR_H_ */
