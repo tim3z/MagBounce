f87f7953a8907bfc62f4b9cbd2c43d4b899d4588
Moritz Baumann <flamingmo@online.de>
2011-12-03 01:48:10 +0100

    Fixed stupid copy&paste bug, things now work as before introducing the new vector class (even better IMHO).

--- src/Game/Model/RectangularLevelObject.cpp
+++ src/Game/Model/RectangularLevelObject.cpp
@@ -10,7 +10,7 @@
 #include "Graphics/RectangularLevelObjectRenderer.h"
 
 RectangularLevelObject::RectangularLevelObject(float x1, float y1, float x2, float y2)
-        : upperLeftCorner(Vector2D(x1, x2)) {
+        : upperLeftCorner(Vector2D(x1, y1)) {
     position = Vector2D(x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2);
     renderer = new RectangularLevelObjectRenderer(abs(x1 - x2), abs(y1 - y2));
 }
